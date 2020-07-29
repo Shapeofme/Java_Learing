3bec260861d6be7c521fe6c384e324d8737b562a
Shapeofme <2377540437@qq.com>
2020-07-29 11:32:47 +0800

    “”

---
+++ 08多线程/src/niu/java/多线程练习/Test.java
@@ -0,0 +1,19 @@
+package niu.java.多线程练习;
+
+import javax.jws.Oneway;
+
+/**
+ * @author niu Email:2377540437@qq.com
+ * @Description
+ * @date ${date} ${time}
+ */
+public class Test {
+    public static void main(String[] args) throws Exception {
+        getname();
+    }
+
+    private static void getname() throws Exception{
+
+    }
+
+}

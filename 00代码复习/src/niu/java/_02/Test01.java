5e9e27122d58d7e04efe5c024d738a0a2546b585
Shapeofme <2377540437@qq.com>
2020-09-02 00:03:57 +0800

    "提交"

--- 00代码复习/src/niu/java/_02/Test01.java
+++ 00代码复习/src/niu/java/_02/Test01.java
@@ -37,8 +37,55 @@ public class Test01 {
         /*boolean b = Boolean.parseBoolean("true");//String 类转换为包装类，之后通过自动装箱转换为基本数据类型
         System.out.println(b);*/
 
-        double a = 10.0/2.3;
-        System.out.println(a);
+        //3.运算符
+        //3-1 算数运算符
+        //小数除法与整数除法的区别
+        /*double a = 10.0/2.3;
+        System.out.println(a);//4.34826...
+        int b = 10/3;
+        System.out.println(b);//3*/
+        //取模:与第一个数的正负有关
+        /*System.out.println(5%-2);//1
+        System.out.println(-5%2);//-1
+        System.out.println(-5%-2);//-1
+        System.out.println(5%2);//1*/
 
+        //3-2 逻辑运算符
+        //测试 &  测试&&
+        /*boolean a = false;
+        if( a==true & (a = true)) ;
+        System.out.println(a); // a = true
+        a=false;
+        if(a == false & (a = true));
+        System.out.println(a);// a = true;
+
+        boolean b = false;
+        if( b == true && (b = true));
+        System.out.println(b); // a = false
+        b = false;
+        if( b == false && (b = true));
+        System.out.println(b); //a = true*/
+
+        /*boolean a = false;
+        if(a == true | (a= true));
+        System.out.println(a);//true
+        a = false;
+        if(a == false | (a = true));
+        System.out.println(a);//true
+
+        boolean b = false;
+        if(b == true || (b = true));
+        System.out.println(b);//true
+        b = false;
+        if(b == false || (b = true));
+        System.out.println(b);//false*/
+
+        //3-3位运算符
+        // >>右移运算符
+        System.out.println(6>>2);// 6/(2*2)
+        System.out.println(-6>>2);// 使用负数的源码进行移位
+        // <<左移运算符
+        System.out.println(6<<2);// 6*(2*2)
+        System.out.println(-6<<2);// -6*(2*2)
     }
 }
